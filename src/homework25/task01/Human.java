22cbc40554d151220d83cb2be467c38832573545
Denis Nejelschi <nejelschi@gmail.com>
2023-11-09 19:01:54 +0100

    task01

---
+++ src/homework25/task01/Human.java
@@ -0,0 +1,11 @@
+package homework25.task01;
+
+public class Human {
+
+  String name;
+  int age;
+
+  public void introduce() {
+    System.out.println("Hi, I am " + name + " I am " + age + " years old.");
+  }
+}
