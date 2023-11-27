9e47bb7a4f318ac1ef2a5d5f7e5cfed102b26653
Denis Nejelschi <nejelschi@gmail.com>
2023-11-27 21:41:57 +0100

    task 3 solve

--- src/homework33/task03/Main.java
+++ src/homework33/task03/Main.java
@@ -2,7 +2,7 @@ package homework33.task03;
 
 import java.util.Scanner;
 
-public class Maingit
+public class Main{
 
   public static int[] getFibonacci(int n) {
 
@@ -12,20 +12,23 @@ public class Maingit
     for (int i = 2; i < array.length; i++) {
       array[i]= array[i-1]+array[i-2];
     }
-    for (int i = 0; i < array.length; i++)
-      System.out.print(array[i] + "| ");
+
 
     return array;
   }
-
-
-
-  public static void main(String[] args) {
-    System.out.println("how many numbers you need ?");
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-    getFibonacci(n);
-
+  public static void printArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++)
+      System.out.print(arr[i] + " ");
   }
 
-}
+    public static void main (String[]args){
+      System.out.println("how many numbers you need ?");
+      Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt();
+
+      int[] fib = getFibonacci(n);
+      printArray(fib);
+
+
+    }
+  }
