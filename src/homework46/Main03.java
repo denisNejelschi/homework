9c33365ed161d4c44862edfc72fd8c7637682f2a
Denis Nejelschi <nejelschi@gmail.com>
2024-01-11 18:50:34 +0100

    homework 46 temp

--- src/homework46/Main03.java
+++ src/homework46/Main03.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 
 public class Main03 {
 
-  public static void  fillRandomArray( int[] arr){
+  public static void fillRandomArray(int[] arr) {
     Random random = new Random();
-    for (int i = 0; i < arr.length ; i++) {
+    for (int i = 0; i < arr.length; i++) {
       arr[i] = random.nextInt(100);
     }
 
   }
-  public static void printArr(int[]arr){
+
+  public static void printArr(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i]+ "|");
+      System.out.print(arr[i] + "|");
     }
   }
-  public static void sortDescending(int[] arr){ //честно не сам написал. подсмотрел.
+
+  public static void sortDescending(int[] arr) { //честно не сам написал. подсмотрел.
     for (int i = 0; i < arr.length - 1; i++) {
       for (int j = 0; j < arr.length - i - 1; j++) {
 
@@ -33,17 +35,15 @@ public class Main03 {
   }
 
 
-
-
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("how long the array should be, enter it's length : ");
     int[] arr = new int[scanner.nextInt()];
-fillRandomArray(arr);
-printArr(arr);
-sortDescending(arr);
+    fillRandomArray(arr);
+    printArr(arr);
+    sortDescending(arr);
     System.out.println("\nSorted :");
-printArr(arr);
+    printArr(arr);
 
 
   }
